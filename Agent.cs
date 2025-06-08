bb9d1fc05b6f55868c5a707d8812a4db1f5bb72f
Yonatan Rosenfeld <yonatan101200@gmail.com>
2025-06-08 18:05:42 +0300

    Add project files.

---
+++ Agent.cs
@@ -0,0 +1,20 @@
+﻿using MySqlX.XDevAPI.Relational;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AgentsDB
+{
+    internal class Agent
+    {
+        private int id {  get; set; }
+        private string codeName { get; set; }
+        private string realName { get; set; }
+        private string location { get; set; }
+        private string status { get; set; }
+        private int missionsCompleted { get; set; }
+    }
+}
